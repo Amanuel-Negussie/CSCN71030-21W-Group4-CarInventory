2d002b27b42f34958b0c614e24d3844b39d798ff
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2021-03-11 18:11:20 -0500

    added SAVELOAD

--- CSCN71030-21W-Negussie/SaveLoad.h
+++ CSCN71030-21W-Negussie/SaveLoad.h
@@ -6,6 +6,19 @@ using namespace std;
 void SAVE(void* addressOfObject, const int sizeOfClass,ofstream& in);
 void LOAD(void* addressOfObject, const int sizeOfClass,ifstream& out);
 
+class SAVELOAD {
+public:
+	virtual void save(ofstream& in) = 0;
+	virtual void load(ifstream& out) = 0;
+};
+
+class SAVELOAD {
+public:
+	virtual void save(ofstream& in) = 0;
+	virtual void load(ifstream& out) = 0;
+};
+
+
 
 
 
