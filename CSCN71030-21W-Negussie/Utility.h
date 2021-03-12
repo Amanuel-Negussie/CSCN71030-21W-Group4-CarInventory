f0c940364c6b2af96e34f22418f5a9908f956476
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2021-03-12 01:28:46 -0500

    Updated Utility

---
+++ CSCN71030-21W-Negussie/Utility.h
@@ -0,0 +1,11 @@
+/*
+Utility is an abstract class that will be used by objects that need to display their contents
+Such as Vehicle, Customer and User
+*/
+#pragma once
+class Utility
+{
+public:
+	virtual void print(void) = 0;
+};
+
