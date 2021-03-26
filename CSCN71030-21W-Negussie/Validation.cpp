f389cabc795823bf41dd095abca93148b5dc755a
Jacky Lam <Jlam5676@conestogac.on.ca>
2021-03-26 10:46:07 -0400

    Validation first draft completed.

--- CSCN71030-21W-Negussie/Validation.cpp
+++ CSCN71030-21W-Negussie/Validation.cpp
@@ -1,1 +1,24 @@
 #include "Validation.h"
+
+bool doesStringOnlyContain(const string& s, unsigned int DataType)
+{
+	int strLen = s.length();
+	unsigned int stringContains = 0;
+	for (int i = 0; i < strLen; i++)
+	{
+		if ((stringContains & NUMBER) == 0 && s[i] >= '0' && s[i] <= '9') {
+			stringContains += NUMBER;
+		}
+		else if ((stringContains & SYMBOL) == 0) {
+			stringContains += SYMBOL;
+		}
+		else if ((stringContains & LETTER) == 0) {
+			stringContains += LETTER;
+		}
+		else if ((stringContains & SPACE) == 0) {
+			stringContains += SPACE;
+		}
+
+	}
+	return stringContains & DataType;
+}
--- CSCN71030-21W-Negussie/Validation.h
+++ CSCN71030-21W-Negussie/Validation.h
@@ -1,5 +1,15 @@
 #pragma once
-class Validation
-{
+#include <iostream>
+using namespace std;
+
+enum DataType {
+	NUMBER = 0x1,
+	SYMBOL = 0x2,
+	LETTER = 0x4,
+	LWCASE = 0x8,
+	UPCASE = 0x10,
+	SPACE = 0x20
 };
+
+bool doesStringOnlyContain(const string& s, unsigned int DataType);
 
