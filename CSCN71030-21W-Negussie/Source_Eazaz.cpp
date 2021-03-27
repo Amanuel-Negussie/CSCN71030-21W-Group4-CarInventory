1f15383c7f9cd8ca66e8f2fd716b555c93d0bfc5
Eazaz Jakda <Ejakda2584@conestogac.on.ca>
2021-03-27 15:38:47 -0400

    Added User classes and functions. Also added the verification function.

---
+++ CSCN71030-21W-Negussie/Source_Eazaz.cpp
@@ -0,0 +1,92 @@
+//Eazaz Jakda
+//
+//Main to test User and verification functions
+//
+
+
+
+
+#include <iostream>
+#include <stdbool.h>
+
+using namespace std;
+
+#include "Users.h"
+
+
+int main()
+{
+	initializeElementsUserArray();
+
+	Admin a;		//create uninitialized Admin
+
+	//Create Admin
+
+	char fname[] = "Eazaz";
+	char pass[] = "Password1";
+	char userID[] = "EazazJ1";
+
+	Admin b(fname, pass, userID);
+
+	//Create another Admin
+
+	char fname1[] = "Maaz";
+	char pass1[] = "Password123";
+	char userID1[] = "MaazJ1";
+
+	Admin Maaz(fname1, pass1, userID1);
+
+	Manager c;		//create uninitialized Manager
+
+	//Create Manager
+
+	char fname2[] = "The Manager";
+	char pass2[] = "Password_Manager";
+	char userID2[] = "Manager1";
+
+	Manager d(fname2, pass2, userID2);
+
+	//create salesRep
+
+	char fname3[] = "The Salesrep";
+	char pass3[] = "Password_Salesrep";
+	char userID3[] = "Salesrep1";
+
+	SalesRep n(fname3, pass3, userID3);
+
+
+	b.displayUser();
+	
+	Maaz.displayUser();
+
+	d.displayUser();
+
+	n.displayUser();
+
+
+	//Chnage username
+
+	char fnamechanged[] = "EazazChanged";
+
+	b.changeUsername(fnamechanged);
+
+	b.displayUser();
+		
+	//Check to see if the username and password are the same as what is saved in the 2d array
+
+	if (verifyCredentials(fnamechanged, pass))
+	{
+		cout << "\nVerified!" << endl;
+	}
+	else
+	{
+		cout << "\nIncorrect username or password!\n" << endl;
+	}
+
+	
+	//Display array to make sure that the usersa are being added correctly
+
+	displayArray();
+
+	return 0;
+}
