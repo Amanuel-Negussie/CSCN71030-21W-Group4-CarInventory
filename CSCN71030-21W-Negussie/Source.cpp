1001adf2861835ef39272e7f49ac46a9d0d247e9
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2021-04-11 20:18:23 -0400

    yo

--- CSCN71030-21W-Negussie/Source.cpp
+++ CSCN71030-21W-Negussie/Source.cpp
@@ -7,10 +7,28 @@ CSCN7103021W-Negussie
 
 using namespace std;
 
-
 int main(void) {
 
+	char firstName[] = "Hanan";
+	char lastName[] = "Mohammed";
+	char secondFirstName[] = "WEiRDO";
+	char secondLastName[] = "BOOGER";
+	char longFName[] = "12345678901234567890123456789";
 
+
+	Customer longCustomer(longFName, longFName, INT_MAX);
+	Customer Hanan(firstName, lastName, 24);
+	Customer Hanadi(secondFirstName, secondLastName, 25);
+	Hanan.printCustomer(); //print out Hanan
+	Hanadi.printCustomer(); // print out Weirdo
+	string noNameFile{ "Testing" };
+	string textFile{ "Testing.txt" };
+	ofstream out(textFile);
+	Hanadi.save(out);
+	out.close();
+	ifstream in(textFile);
+	Hanan.load(in);
+	in.close();
 
 
 
