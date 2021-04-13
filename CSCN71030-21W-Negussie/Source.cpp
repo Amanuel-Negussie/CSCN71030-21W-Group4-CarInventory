95494dd9cadae15641bd164de1261f16a5039668
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2021-04-12 21:16:54 -0400

    Successfully Tested Integration of Inventory and Save Load Module

--- CSCN71030-21W-Negussie/Source.cpp
+++ CSCN71030-21W-Negussie/Source.cpp
@@ -8,6 +8,7 @@ CSCN7103021W-Negussie
 #include "Customer.h"
 #include "SaveLoad.h"
 #include "Users.h"
+#include "Inventory.h"
 
 using namespace std;
 
@@ -15,6 +16,7 @@ using namespace std;
 int main(void){
     //---------------------------------------------------------------------
 	//Testing Integration for Save/Load Customer
+    //---------------------------------------------------------------------
 	//CUS-SVLD-001 
 	char firstName[] = "John";
 	char lastName[] = "Doe";
@@ -37,9 +39,10 @@ int main(void){
 	//Expectations: Contents of John Doe object that was saved to Customer.txt file will
 	//				be placed into Empty Customer object. Validated by contents printed out Empty Customer print call
 	//RESULT: PASSED 
+    
     //---------------------------------------------------------------------
-
     //Testing Integration for Users and Save/Load
+    //---------------------------------------------------------------------
     //USR-SAV-001
     
     //Load input into variables
@@ -48,14 +51,14 @@ int main(void){
     char userID[] = "EazazJ1";
 
     //Create and open "Users.txt" file to save Users into
-    ofstream userOutputFile("Users.txt");
+    outputFile.open("Users.txt");
 
     //Create a user of type Admin
     Admin Eazaz(username, password, userID);
 
     //Save the Admin object to the file "Users.txt"
-    Eazaz.save(userOutputFile);
-    userOutputFile.close();
+    Eazaz.save(outputFile);
+    outputFile.close();
 
     //Expectations: Users.txt file will be created, and in file User Eazaz
 	//				and its contents will be present
@@ -65,7 +68,7 @@ int main(void){
     //USR-SAV-002
 
     //Open "Users.txt" file to load objects from
-    ifstream userInputFile("Users.txt");
+    inputFile.open("Users.txt");
 
     //Create empty object to load User object from "Users.txt" file into
     Admin EazazLoaded;
@@ -74,18 +77,55 @@ int main(void){
     EazazLoaded.displayUser();
 
     //Load the User object from file into empty User Object
-    EazazLoaded.load(userInputFile);
+    EazazLoaded.load(inputFile);
 
-    userInputFile.close();
+    inputFile.close();
 
     printf("\nAFTER LOAD:\n");
     EazazLoaded.displayUser();
     //Expectations: Contents of Eazaz object that was saved to Users.txt file will
 	//				be placed into Empty Admin object. Validated by contents printed out 
     //Results: PASSED
+    
     //---------------------------------------------------------------------
+    //Testing Integration for Inventory and Save/Load
+    //---------------------------------------------------------------------
+    //INV-SVLD-001
+    char myMake[MAX_LEN] = "Ford";
+   	char myModel[MAX_LEN] = "Escape";
+    char myVIN[MAX_LEN] = "ABC123";
 
 
+    outputFile.open("Inventory.txt");  //ofstream with file name created
+    Inventory myInventory(myMake, myModel, myVIN, SUV, 25999, 19999);
+    myInventory.save(outputFile);
+    outputFile.close();
+    //Expectations: Inventory.txt file will be created, and in file Inventory object "myInventory"
+    //				and its contents will be present
+    //RESULT: PASSED
+    
+
+    //INV-SVLD-002
+    inputFile.open("Inventory.txt");
+
+    //Create empty object to load Inventory object from "Inventory.txt" file into
+    Inventory emptyInventory; 
+
+    printf("\nBEFORE LOAD:\n");
+    emptyInventory.printInventory();
+
+    //Load the User object from file into empty User Object
+    emptyInventory.load(inputFile);
+
+    inputFile.close();
+
+    printf("\nAFTER LOAD:\n");
+    emptyInventory.printInventory();
+    //Expectations: Contents of myInventory object that was saved to Inventory.txt file will
+    //				be placed into Empty Inventory object. Validated by contents printed out
+    //Results: PASSED
+
+    //---------------------------------------------------------------------
 
 	return 0;
 }
